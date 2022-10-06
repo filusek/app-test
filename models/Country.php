c93370fb51b6abcc8704a35b8b5ac60837dc0dd4
filusek <lukasz.firlej25@gmail.com>
2022-10-06 16:08:57 +0200

    crud modelu

---
+++ models/Country.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "country".
+ *
+ * @property int $id
+ */
+class Country extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'country';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+        ];
+    }
+}
